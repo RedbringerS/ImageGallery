9a4b766e72f71113a3804819ab56a5f9452ef730
Ivan Lastouski <redbringers@gmail.com>
2022-06-20 21:31:32 +0300

    Реализован главный экран

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.util.TreeMap;
+
+public class App
 {
+    private static final int WIDTH = 1024;
+    private static final int HEIGHT = 768;
+    //public volatile TreeMap<String, File> imageTreeMap = new TreeMap<>();
+    public void run() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("DT Developer Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        frame.setLayout(new BorderLayout());
+        //frame.add(new OpenImageBrowser());
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SwingUtilities.invokeLater(new App()::run);
     }
 }
